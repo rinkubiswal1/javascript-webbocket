c7f4281fc36c54761ed0603a27a322a963ea71b5
rinkubiswal1 <152254359+rinkubiswal1@users.noreply.github.com>
2023-11-28 07:26:17 +0000

    javascript done

---
+++ 01-basic/text.js
@@ -0,0 +1,9 @@
+console.log("hello world");
+/*
+1. javascriptbis a highlevel scripting language used to perform the task on frontent as well as backend.
+2. javascript use boyh clientside as well as server side.
+3. javascript is most used programing language for ewb development.
+4. in javascript we use "Esb"version is the latest one developed in 2015.
+*Esb=Echmascript 6(vanilla javascript)
+5. javascript is basically used for designing the logic part of our web pages.
+*/
